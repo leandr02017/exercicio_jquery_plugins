6c3d122dd8619446bb482055d4b0d6e6943f6fed
leandrolugotardi66@gmail.com <leandrolugotardi66@gmail.com>
2024-11-12 20:53:17 -0300

    corrigido

--- js/main.js
+++ js/main.js
@@ -1,53 +1,52 @@
 // removi do arquivo html e coloquei aqui
 $(document).ready(function () {
-    $('#carousel').slick({
-        autoplay: true,
-        arrows: false
-    })
-})
+  $("#carousel").slick({
+    autoplay: true,
+    arrows: false,
+  });
+});
 
 //tava mascara
-$('#telefone').mask('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+$("#telefone").mask("(00) 00000-0000", {
+  placeholder: "(DDD) 12345-6789",
+});
 
-$('#cpf').mask('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
+$("#cpf").mask("000.000.000-00", {
+  placeholder: "123.456.789-00",
+});
 
-$('#cep').mask('00000-000', {
-    placeholder: '012345-678'
-})
-
+$("#cep").mask("00000-000", {
+  placeholder: "012345-678",
+});
 
 // tava requerido
-$('form').validate({
-    rules: {
-        nome: {
-            required: true
-        },
-        email: {
-            required: true,
-            email: true
-        },
-        telefone: {
-            required: true
-        },
-        endereco: {
-            required: true
-        },
-        cep: {
-            required: true
-        },
-        cpf: {
-            required: true
-        },
+$("form").validate({
+  rules: {
+    nome: {
+      required: true,
     },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
+    email: {
+      required: true,
+      email: true,
     },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
-})
+    telefone: {
+      required: true,
+    },
+    endereco: {
+      required: true,
+    },
+    cep: {
+      required: true,
+    },
+    cpf: {
+      required: true,
+    },
+  },
+  submitHandler: function (form) {
+    alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+    form.reset();
+  },
+  invalidHandler: function (form, validator) {
+    alert("Por favor, preencha os campos para prosseguir com a compra!");
+  },
+});
